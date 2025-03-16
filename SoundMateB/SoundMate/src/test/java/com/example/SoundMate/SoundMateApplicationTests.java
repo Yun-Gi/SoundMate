4b1547ae3ede30e6adc969e0117c4107e4f4a421
윤기 <129282769+Yun-Gi@users.noreply.github.com>
2025-03-16 21:29:29 +0900

    Add files via upload

---
+++ SoundMateB/SoundMate/src/test/java/com/example/SoundMate/SoundMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoundMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
